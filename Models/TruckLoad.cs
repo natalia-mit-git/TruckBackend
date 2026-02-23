1d428e86733bd14d0af2a142f7ec02463a190f50
Natalia Vladykin <vladykin@web.de>
2026-02-23 20:40:06 +0000

    refactor: add parameterized constructor

--- Models/TruckLoad.cs
+++ Models/TruckLoad.cs
@@ -4,11 +4,12 @@ namespace TruckBackend.Models;
 public class TruckLoad
 {
     public int Weight { get; private set; }
-    public string Destination { get; private set; } = "Unknown";
+    public string Destination { get; private set; }
 
-    public TruckLoad()
+    public TruckLoad(int weight = 1000, string destination = "Unknown")
     {
-        Weight = 1000;
+        Weight = weight;
+        Destination = destination;
     }
 
     // Virtual -> polymorphism possible later
