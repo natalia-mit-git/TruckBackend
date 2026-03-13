75bc25e05d88a8a4a39d479c363119dc0f0b0fc3
Natalia Vladykin <vladykin@web.de>
2026-03-13 20:34:16 +0000

    feat: add Truck load management and weight check

--- Models/TruckLoad.cs
+++ Models/TruckLoad.cs
@@ -3,10 +3,12 @@ namespace TruckBackend.Models;
 // Represents cargo (data + behavior)
 public class TruckLoad
 {
+    public const int DefaultWeight = 1000;
+    public const string DefaultDestination = "Unknown";
     public int Weight { get; private set; }
     public string Destination { get; private set; }
 
-    public TruckLoad(int weight = 1000, string destination = "Unknown")
+    public TruckLoad(int weight, string destination)
     {
         Weight = weight;
         Destination = destination;
