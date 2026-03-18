633ff987b3245b2e66caea1f47bdab9fa0996cda
Natalia Vladykin <vladykin@web.de>
2026-03-18 21:01:24 +0000

    feat(app): register TruckContext with PostgreSQL connection

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 using TruckBackend.Services;
+using TruckBackend.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = "Host=postgres;Database=truckdb;Username=postgres;Password=postgres";
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ShippingService>();
+builder.Services.AddDbContext<TruckContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
