542597d56fdd36a4d008943ede41372eb6fef655
Natalia Vladykin <vladykin@web.de>
2026-03-19 11:23:00 +0000

    fix(model): resolve nullable reference warnings in Truck

--- Models/Truck.cs
+++ Models/Truck.cs
@@ -4,8 +4,8 @@ namespace TruckBackend.Models;
 public class Truck
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public ICollection<TruckLoad> Loads { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public ICollection<TruckLoad> Loads { get; set; } = new List<TruckLoad>();
 
     private const int MaxCapacity = 10000;
     private List<TruckLoad> _loads = new();
