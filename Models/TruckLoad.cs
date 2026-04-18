07e5a79401b285477f04561479349240f20e9848
Natalia Vladykin <vladykin@web.de>
2026-04-18 16:26:20 +0000

    refactor(model): set default values for TruckLoad properties in constructor

--- Models/TruckLoad.cs
+++ Models/TruckLoad.cs
@@ -7,13 +7,12 @@ public class TruckLoad
     public const string DefaultDestination = "Unknown";
 
     public int Id { get; set; }
-    public int Weight { get; set; }
-    public string Destination { get; set; }
+    public int Weight { get; set; } = DefaultWeight;
+    public string Destination { get; set; } = DefaultDestination;
 
     public int TruckId { get; set; }
     public Truck? Truck { get; set; }
-
-    public TruckLoad(int truckId, int weight, string destination)
+    public TruckLoad(int truckId, int weight = DefaultWeight, string destination = DefaultDestination)
     {
         TruckId = truckId;
         Weight = weight;
